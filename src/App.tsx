ebdee1ea541592f62aac6cec98ae4929b10c4dc6
Chenzhumei <zcm_1018@163.com>
2022-02-08 14:40:21 +0800

    menu menuItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import {useState} from 'react'
 import Button, {ButtonType, ButtonSize} from './components/Button/button'
+import Menu, {MenuProps} from './components/Menu/menu';
+import MenuItem, {MenuItemProps} from './components/Menu/menuItem';
 
 function App() {
  
   return (
     <div className="App">
+      <Menu defaultIndex={0} mode={'vertical'} onSelect={(index) => {alert(index)}}>
+         <MenuItem>
+            cool link
+         </MenuItem>
+         <MenuItem disabled>
+            cool link 2
+         </MenuItem>
+         <MenuItem>
+            cool link 3
+         </MenuItem>
+      </Menu>
+
+
+
       <Button className='custom' onClick={(e) => {e.preventDefault();alert(123)}}>hello</Button>
       <Button disabled>disabled button</Button>
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>large primary</Button>
